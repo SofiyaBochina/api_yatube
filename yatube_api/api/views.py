eb841db6a0d17f2ee3835ec180a7fb7a48c20ce9
Sofiya Bochina <drastnikova@gmail.com>
2022-02-02 17:35:01 +0300

    Docstrings added (sprint 8)

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -10,20 +10,24 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
     def perform_create(self, serializer):
+        """Сохранение пользователя в поле 'author'."""
         serializer.save(author=self.request.user)
 
     def perform_destroy(self, serializer):
+        """Проверка на права при удалении поста."""
         if serializer.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super(PostViewSet, self).perform_destroy(serializer)
 
     def perform_update(self, serializer):
+        """Проверка на права при изменении поста."""
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super(PostViewSet, self).perform_update(serializer)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
@@ -32,11 +36,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
     def get_queryset(self):
+        """Получение списка комментов к определенному посту."""
         post_id = self.kwargs.get('post_id')
         new_queryset = Comment.objects.filter(post=post_id)
         return new_queryset
 
     def perform_create(self, serializer):
+        """Сохранение поста и пользователя в полях 'post' и 'author'."""
         post = Post.objects.get(id=self.kwargs.get('post_id'))
         serializer.save(
             author=self.request.user,
@@ -44,11 +50,13 @@ class CommentViewSet(viewsets.ModelViewSet):
         )
 
     def perform_destroy(self, serializer):
+        """Проверка на права при удалении коммента."""
         if serializer.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super(CommentViewSet, self).perform_destroy(serializer)
 
     def perform_update(self, serializer):
+        """Проверка на права при изменении коммента."""
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super(CommentViewSet, self).perform_update(serializer)
