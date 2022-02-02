03e5d17794ad4e11f9c48cd5bc55376d64344408
Sofiya Bochina <drastnikova@gmail.com>
2022-02-02 17:22:39 +0300

    First try (sprint 1)

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,12 +1,25 @@
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+router = routers.DefaultRouter()
+router.register('posts', PostViewSet)
+router.register('groups', GroupViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(
